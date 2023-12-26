a0384773033ece2f3ba3274d16cf4662c35df46b
Will Mattos <willmattos.c@gmail.com>
2023-12-26 10:47:20 +0100

    Update RatemoviesController.php

--- src/Controller/RatemoviesController.php
+++ src/Controller/RatemoviesController.php
@@ -183,6 +183,7 @@ class RatemoviesController extends AbstractController
     #[Route('/prueba', name: 'prueba')]
     public function prueba()
     {
+        phpinfo();
         echo ini_get('upload_max_filesize'); // Debería imprimir el valor configurado
         echo ini_get('post_max_size');
         die;
